73ca60c7ea162fb451bc13adb07e0cbf22c567b8
trukozak <aleksandr.nepijvoda@gmail.com>
2021-07-20 09:02:50 +0300

    finish

---
+++ src/Components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderList from "./headerList/HeaderList";
+
+const Header = () => {
+  return (
+    <nav>
+      <HeaderList />
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/Components/main/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import data from "../../data";
+import LaptopList from "../laptopList/LaptopList";
+import PhoneList from "../phoneList/PhoneList";
+import Section from "../section/Section";
+
+const Main = () => {
+  return (
+    <>
+      <Section title={"Мобильные телефоны"}>
+        <PhoneList phones={data.phones} />
+      </Section>
+      <Section title={"Ноутбуки"}>
+        <LaptopList laptops={data.laptops} />
+      </Section>
+    </>
+  );
+};
+
+export default Main;
